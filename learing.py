5449d60e72cdc779c7a345909089bde06776abab
AkilSweidan <136123257+AkilSweidan@users.noreply.github.com>
2024-12-27 11:56:15 +0200

    Add files via upload
    Hello, I started learning Python .

---
+++ learing.py
@@ -0,0 +1,8 @@
+print("Helllo python")
+# am new and strating to learn new things 
+print(" am now in vid number 7")
+print(type(1))
+print(type(1.0555))
+print(type([1,2,3,4]))
+print(type({1 : "one" , 2 : "two"} ))
+print(type((1,2,3,4)))
