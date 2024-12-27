7d815ef96269575b850a053632856a489409903e
AkilSweidan <136123257+AkilSweidan@users.noreply.github.com>
2024-12-27 12:54:20 +0200

    learning.py
    finished vid 7,8,9

--- learing.py
+++ learing.py
@@ -6,3 +6,30 @@ print(type(1.0555))
 print(type([1,2,3,4]))
 print(type({1 : "one" , 2 : "two"} ))
 print(type((1,2,3,4)))
+print("Her is the link of the course that am learning python with : https://youtube.com/playlist?list=PLDoPjvoNmBAyE_gei5d18qkfIe-Z8mocs&si=88T2AcsicBXl5M_J")
+print("I finished vid 7 and I will start with 8 ")
+#--------vid 7 summery : Variables
+myname = "Akil Sweidan"
+print(myname)
+myage = "17"
+print(myage)
+#-------vid 8 summery : Variables Part Two
+x = "10"
+x = "age" 
+print(x)
+help("keywords")
+a ,b ,c = 1,2,3
+print(a)
+print(b)
+print(c)
+c ,l ,k = "CC", "ll", "kk"
+print((c) , (l) ,(k))
+print("I finished vid 8 and will start with 9")
+#------vid 9 summery : Escape Sequences Characters
+print("Hello\bPython")
+print("Hello \
+Python")
+print("I\
+ love \
+Python")
+print('Hello' 'Python')
