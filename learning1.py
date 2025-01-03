f0b93a3a2973c23fb89f14d3d7ab110361a4bdb3
AkilSweidan <136123257+AkilSweidan@users.noreply.github.com>
2025-01-03 16:34:15 +0200

    Add files via upload

---
+++ learning1.py
@@ -0,0 +1,18 @@
+# Learning python part 2 
+x = "Hello"
+y = "Hello"
+print( x == y )
+print ( x != y )
+print(x < y )
+print("What is your name ?")
+A = input(" ")
+if A == "Akil": 
+    print("You are Real Human ")
+else:
+    print("You are not Real Human")
+x = ["AKIL", 4, 7]
+print(len(x))
+x.append("LFG")
+print(x)
+x.extend([7, 7.070,"to"])
+print(x)
