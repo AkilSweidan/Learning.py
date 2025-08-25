f097b037320c489bb56c5e6527803958e433ee45
AkilSweidan <136123257+AkilSweidan@users.noreply.github.com>
2025-08-26 00:23:15 +0300

    Grade Converter
    Convert your Grade from Numbers to Letters

---
+++ grade.py
@@ -0,0 +1,11 @@
+score = float(input(" What is your score ? "))
+if score <= 100 and score >= 90 :
+    print("Grade : A ")
+elif score <90 and score >= 80 :
+    print("Grade : B ")
+elif score  <80  and score >= 70 :
+    print("Grade : C ") 
+elif score <70 and score >= 60 :
+    print("Grade : D ")  
+else :
+    print("Grade : F ")
