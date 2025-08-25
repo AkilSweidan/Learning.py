0d28909482a75acf18d55de735e986afa9f587c1
AkilSweidan <136123257+AkilSweidan@users.noreply.github.com>
2025-08-26 00:18:37 +0300

    Odd or Even identifier

---
+++ compare.py
@@ -0,0 +1,10 @@
+x = int(input("What's X ?  "))
+y = int(input("What's Y ?  "))
+
+if x > y :
+    print("X greater then Y")
+elif  x < y :
+    print("X smaller then  Y")
+else :
+    print("X is eaual to Y")
+   
