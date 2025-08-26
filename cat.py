d0d38b17e614c05921dd624614c7394937f23be4
AkilSweidan <136123257+AkilSweidan@users.noreply.github.com>
2025-08-27 00:46:35 +0300

    Cat Speak
    your asked to enter a positive integer , then the cat will say Meow according to the positive integer you entered

---
+++ cat.py
@@ -0,0 +1,9 @@
+while True :
+    x = int(input("how many meows do you want? "))
+    if x > 0 :
+        break
+    else :
+        print("Please enter a positeive integer .")
+        
+for _ in range(x) :
+    print("meow " )
